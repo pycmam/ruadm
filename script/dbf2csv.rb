a4935278c09a32c6788c45cf7ad944f409be0245
zverok <zverok.offline@gmail.com>
2016-01-27 00:44:59 +0200

    Sorry

--- script/dbf2csv.rb
+++ script/dbf2csv.rb
@@ -9,7 +9,7 @@ FileUtils.mkdir_p 'script/data'
 
 CSV.open("script/data/kladr.csv", "wb") do |csv|
   table.each do |rec|
-    next if rec.code.start_with?('91')
+    next if rec.code.start_with?('91') || rec.code.start_with?('92')
     csv << [rec.name.force_encoding('CP866').encode('UTF-8'), rec.code]
   end
 end
